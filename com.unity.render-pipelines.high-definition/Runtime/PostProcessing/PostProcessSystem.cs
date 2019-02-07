5a96d24e11a353a8c4b523e3ebff518256399ebe
FrancescoC-Unity <francescoc@unity3d.com>
2019-02-07 15:05:43 +0100

    Disable motion blur if game is not playing as velocity vectors are not reliable.

--- com.unity.render-pipelines.high-definition/Runtime/PostProcessing/PostProcessSystem.cs
+++ com.unity.render-pipelines.high-definition/Runtime/PostProcessing/PostProcessSystem.cs
@@ -325,7 +325,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
 
                     // Motion blur after depth of field for aesthetic reasons (better to see motion
                     // blurred bokeh rather than out of focus motion blur)
-                    if (m_MotionBlur.IsActive() && camera.camera.cameraType == CameraType.Game && !m_ResetHistory)
+                    if (m_MotionBlur.IsActive() && camera.camera.cameraType == CameraType.Game && !m_ResetHistory && Application.isPlaying)
                     {
                         using (new ProfilingSample(cmd, "Motion Blur", CustomSamplerId.MotionBlur.GetSampler()))
                         {
